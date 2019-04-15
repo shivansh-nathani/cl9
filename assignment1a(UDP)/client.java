42e39ce6d870094e72a127a1ba7392bde2d2be17
shivansh9007 <nathanishivansh07@gmail.com>
2019-04-15 22:42:57 +0530

    multiple udp

--- assignment1a(UDP)/client.java
+++ assignment1a(UDP)/client.java
@@ -1,14 +1,20 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-
+import java.util.*;
 class client{
     public static void main(String[] args) throws Exception{
         DatagramSocket s =new DatagramSocket();
-        byte[] buffer  = new String("hey").getBytes();
-        System.out.println(buffer[0]);
+        System.out.println("Enter the text " );
+        Scanner sc = new Scanner(System.in);
+        byte[] buffer = sc.nextLine().getBytes();
+
+        //System.out.println(buffer[0]);
         DatagramPacket data  = new DatagramPacket(buffer, buffer.length,InetAddress.getByName("localhost"),6555);
         s.send(data);
+         data = new DatagramPacket(buffer, buffer.length);
+        	s.receive(data);
+        	System.out.println("data from server" +new String(data.getData()));
         s.close();
     }
 }
--- assignment1a(UDP)/server.java
+++ assignment1a(UDP)/server.java
@@ -1,13 +1,24 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-
+import java.net.InetAddress;
 class server{
     public static void main(String[] args) throws Exception{
         DatagramSocket s = new DatagramSocket(6555);
         byte[] buffer = new byte[1000];
         //System.out.println(buffer.length);
-        DatagramPacket data = new DatagramPacket(buffer, buffer.length);
-        s.receive(data);
-        System.out.print(new String(data.getData()));
+        while(true)
+        {
+        	DatagramPacket data = new DatagramPacket(buffer, buffer.length);
+        	s.receive(data);
+        	String d= new String(data.getData());
+        	
+        	 buffer = d.getBytes();
+        	 InetAddress add = data.getAddress();
+        	 int port = data.getPort();
+        	 System.out.print("sending data to client "+d+"port "+port+"addr "+add);
+        	 data  = new DatagramPacket(buffer, buffer.length,add,port);
+        	s.send(data);
+        }
+        
     }
 }
